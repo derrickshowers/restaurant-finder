0705a8d888b02ac59b8772fc84456e70dad1f896
Derrick Showers <derrick@derrickshowers.com>
2014-02-27 01:03:50 -0800

    added select box for city choice

--- src/js/view.js
+++ src/js/view.js
@@ -5,19 +5,25 @@ restaurantApp.Views.RestaurantListView = Backbone.View.extend({
 	
 	tagName: 'div',
 
-	className: 'list-group',
-
 	template: _.template(restaurantApp.Templates.RestaurantListTemplate),
+
+	events: {
+		'change select'		: 'render'
+	},
 
 	initialize: function() {
 		this.collection.on('reset', this.render, this);
 	},
 
 	render: function() {
-		var $template = $('<div/>');
 
+		var selectedCity = $('#selectCity').val() || 'all',
+			$template = $('<div/>');
+		
 		// check to see if model's selected property is true.
-		// if so, add class 'active'
+		// if so, add class 'active'. Also, only display restaurant
+		// if it's in the selected city.
+		$template.addClass('list-group');
 		this.collection.forEach(function(restaurant) {
 			$item = $(this.template(restaurant.toJSON()));
 			if (restaurant.selected) {
@@ -25,12 +31,18 @@ restaurantApp.Views.RestaurantListView = Backbone.View.extend({
 			} else {
 				$item.removeClass('active');
 			}
-			$template.append($item);
+			if (selectedCity === 'all' || selectedCity === restaurant.get('city')) {
+				$template.append($item);
+			}
 		}, this);
+
+		// render the select box and choose current city
+		this.$el.html(this.buildSelect());
+		$('#selectCity').val(selectedCity);
 
 		// now let's add the childen of the jQuery template
 		// object to the view's el
-		this.$el.html($template.children());
+		this.$el.append($template);
 	},
 
 	// called to change the selected item on the list.
@@ -46,6 +58,36 @@ restaurantApp.Views.RestaurantListView = Backbone.View.extend({
 			}
 		}, this);
 		this.render();
+	},
+
+	// build the select box by checking collection for
+	// any and all cities.
+	buildSelect: function() {
+		var $select = $('<select/>').addClass('form-control').attr('id', 'selectCity'),
+			cities = [];
+		
+		// loop through the models to build an array of cities
+		$select.append('<option value="all">See All Cities</option>')
+		this.collection.forEach(function(restaurant) {
+			var city = restaurant.get('city');
+			var isNew = true;
+			for (var i=0; i<cities.length; i++) {
+				if (cities[i] === city) {
+					isNew = false;
+				}
+			}
+			if (isNew) {
+				cities.push(city);
+			}
+		});
+		
+		// add the options to the $select el
+		cities.forEach(function(city) {
+			var option = $('<option value="' + city + '">' + city + '</option>');
+			$select.append(option);
+		});
+
+		return $select;
 	}
 
 });
