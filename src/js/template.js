90b6f7785e0cc2a4243db953604c786055ce3e3d
Derrick Showers <derrick@derrickshowers.com>
2014-03-01 19:30:30 -0800

    added ability to update a restaurant

--- src/js/template.js
+++ src/js/template.js
@@ -32,28 +32,53 @@ restaurantApp.Templates.RestaurantDetailTemplate = '\
 
 restaurantApp.Templates.RestaurantAddNewTemplate = '\
 	<div class="form-group">\
-		<label for="exampleInputEmail1">Restaurant Name</label>\
+		<label for="name">Restaurant Name</label>\
 		<input id="name" class="form-control" type="text" placeholder="Ex. Bob\' Big Boy">\
 	</div>\
 	<div class="form-group">\
-		<label for="exampleInputEmail1">Restaurant\'s Street Address</label>\
+		<label for="address">Restaurant\'s Street Address</label>\
 		<input id="address" class="form-control" type="text" placeholder="Ex. 1 Infinite Loop">\
 	</div>\
 	<div class="form-group">\
-		<label for="exampleInputEmail1">Restaurant\'s City</label>\
+		<label for="city">Restaurant\'s City</label>\
 		<input id="city" class="form-control" type="text" placeholder="Ex. San Jose">\
 	</div>\
 	<div class="form-group">\
-		<label for="exampleInputEmail1">Favorite Entrée</label>\
+		<label for="favoriteEntree">Favorite Entrée</label>\
 		<input id="favoriteEntree" class="form-control" type="text" placeholder="Ex. San Jose">\
 	</div>\
 	<div class="form-group">\
-		<label for="exampleInputEmail1">Favorite Drink</label>\
+		<label for="favoriteDrink">Favorite Drink</label>\
 		<input id="favoriteDrink" class="form-control" type="text" placeholder="Ex. San Jose">\
+	</div>\
+	<button type="submit" class="btn btn-success">Save It</button>\
+';
+
+restaurantApp.Templates.RestaurantEditTemplate = '\
+	<div class="form-group">\
+		<label for="name">Restaurant Name</label>\
+		<input <% if (name !== undefined) { %>value="<%= name %>"<% } %> id="name" class="form-control" type="text" placeholder="Ex. Bob\' Big Boy">\
+	</div>\
+	<div class="form-group">\
+		<label for="address">Restaurant\'s Street Address</label>\
+		<input <% if (address !== undefined) { %>value="<%= address %>"<% } %> id="address" class="form-control" type="text" placeholder="Ex. 1 Infinite Loop">\
+	</div>\
+	<div class="form-group">\
+		<label for="city">Restaurant\'s City</label>\
+		<input <% if (city !== undefined) { %>value="<%= city %>"<% } %> id="city" class="form-control" type="text" placeholder="Ex. San Jose">\
+	</div>\
+	<div class="form-group">\
+		<label for="favoriteEntree">Favorite Entrée</label>\
+		<input <% if (favorites[0].name !== undefined) { %>value="<%= favorites[0].name %>"<% } %> id="favoriteEntree" class="form-control" type="text" placeholder="Ex. San Jose">\
+	</div>\
+	<div class="form-group">\
+		<label for="favoriteDrink">Favorite Drink</label>\
+		<input <% if (favorites[1].name !== undefined) { %>value="<%= favorites[1].name %>"<% } %> id="favoriteDrink" class="form-control" type="text" placeholder="Ex. San Jose">\
 	</div>\
 	<button type="submit" class="btn btn-success">Save It</button>\
 ';
 
 restaurantApp.Templates.Controls = '\
 	<button id="addNew" type="button" class="btn btn-primary">New Restaurant</button>\
+	<button id="edit" type="button" class="btn btn-primary">Edit This Restaurant</button>\
 ';
