5cdcf21fa307f92d0395842bb609d9b905778463
Derrick Showers <derrick@derrickshowers.com>
2014-03-01 17:12:02 -0800

    adding default image if restaurant doesn't have one

--- src/js/model.js
+++ src/js/model.js
@@ -5,7 +5,8 @@ restaurantApp.Models.Restaurant = Backbone.Model.extend({
 	defaults: {
 		name		: 'Unknown Name',
 		city		: 'Unknown City',
-		address		: 'Unknown Address'
+		address		: 'Unknown Address',
+		main_image	: 'missing-image.svg'
 	},
 	selected: false
 });
