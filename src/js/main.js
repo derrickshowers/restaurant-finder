9000084235f535137835a31201fbc35ffbc17821
Derrick Showers <derrick_showers@apple.com>
2014-03-17 15:43:19 -0700

    fixing loading issue with deps

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,24 @@ var restaurantApp = restaurantApp || {};
 
 require.config({
 	paths: {
-		jquery: 	'vendor/jquery-2.1.0.min',
-		backbone: 	'vendor/backbone',
+		jquery: 'vendor/jquery-2.1.0.min',
+		backbone: 'vendor/backbone',
 		underscore: 'vendor/underscore',
-		bootstrap: 	'vendor/bootstrap', 
+		bootstrap: 'vendor/bootstrap', 
+	},
+	shim: {
+		backbone: {
+			deps: ['underscore', 'jquery']
+		},
+		bootstrap: {
+			deps: ['jquery']
+		},
+		app: {
+			deps: ['backbone']
+		}
 	}
 })
 
-require(['jquery', 'backbone', 'underscore', 'bootstrap', 'app'], function() {
+require(['jquery', 'underscore', 'backbone', 'bootstrap', 'app'], function() {
 	restaurantApp.app.start();
 });
